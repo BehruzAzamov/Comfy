62402d3b1ba87cf4741ecd19f50e9290fdba0652
azamovbehruz <azamovbehruz@gmail.com>
2024-05-15 08:18:31 +0500

    Ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase/firebaseConfig";
 import { signOut } from "firebase/auth";
+
 const Header = () => {
-  const { user } = useSelector((state) => state.userState);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        setCurrentUser(null);
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
+
   return (
     <header className=" bg-neutral py-2 text-neutral-content">
       <div className="align-element flex justify-center sm:justify-end">
         <div className="flex gap-x-6 justify-center items-center sm:justify-end">
-          {!user && (
+          {!currentUser ? (
             <>
               <Link className="link link-hover text-xl sm:text-sm" to="/login">
-                Sign in/ Guest
+                Sign in / Guest
               </Link>
               <Link
                 className="link link-hover text-xl sm:text-sm"
@@ -21,11 +40,10 @@ const Header = () => {
                 Create Account
               </Link>
             </>
-          )}
-          {user && (
+          ) : (
             <>
-              <p>{`hi ${user.displayName}`}</p>
-              <button onClick={() => signOut(auth)} className="btn btn-sm ">
+              <p>{`Hello, ${currentUser.displayName}`}</p>
+              <button onClick={handleLogout} className="btn btn-sm">
                 Logout
               </button>
             </>
